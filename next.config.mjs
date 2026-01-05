ae2c7bb6502e3e652ff5f17287165929716e8f1f
amanyoud737-design <amanyoud737@gmail.com>
2026-01-05 20:04:21 +0300

    next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { serverActions: { allowedOrigins: [] } },
+  images: {
+    remotePatterns: [],
+  },
+};
+export default nextConfig;
